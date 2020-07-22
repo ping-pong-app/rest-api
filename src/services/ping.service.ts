c92e792a826ff450cb2f22db60cae3c7d1c9c8a9
Jamsek-m <miha_jamsek@windowslive.com>
2020-07-22 16:40:32 +0200

    added validation

--- src/services/ping.service.ts
+++ src/services/ping.service.ts
@@ -1,10 +1,14 @@
 import { NotFoundError, Ping } from "../lib";
 import { FirebaseService } from "./firebase.service";
 import { GroupsService } from "./groups.service";
+import { Validator } from "./validator";
 
 export class PingService {
     
     public static async pingGroup(ping: Ping, userId: string) {
+        Validator.assertNotNull(ping);
+        Validator.assertNotBlank(ping.groupId);
+        
         if (await GroupsService.isMemberOfGroup(userId, ping.groupId)) {
             const topic = `PING.${ping.groupId}`;
             const data = {
